641718f1acb86c465771894bc5c41786f81e86de
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2021-07-26 14:15:11 +0100

    Updated Tracker

--- daily_twitter_sample/track.py
+++ daily_twitter_sample/track.py
@@ -1,5 +1,9 @@
-from utils import get_date_range, twitter_date_format_to_day, db
-from count_or_search import count, use_x_as_id, form_count_query_params
+from datetime import datetime
+from random import randrange
+
+from utils import get_date_range, twitter_date_format_to_day, get_month_array
+from decorators import db, accept_duplicates, time_func
+from count_or_search import count, form_count_query_params
 
 
 def track_months(query: str, month: list):
@@ -20,19 +24,67 @@ def track_months(query: str, month: list):
         print(f"{month} was already saved in MongoDB counts")
 
 
-def save_counts(query: str, months: list):
-    # pull count of tweets for a month
+def check_counts(months_range: list):
+    tracker = db["counts"].find_one({"_id": "track"}, {"months": 1, "_id": 0})
 
-    first, last = get_date_range(months)
+    month_array = get_month_array(months_range)
+    tracked_before, tracked_after = [], []
+    first_untracked, last_untracked = None, None
+
+    for month in month_array:
+        last_month = month
+
+        if month in tracker:
+            if first_untracked is None:
+                tracked_before.append(month)
+            elif last_untracked is not None:
+                tracked_after = []
+        else:
+            if first_untracked is None:
+                first_untracked = month
+            elif last_untracked is None or tracked_after is []:
+                last_untracked = last_month
+                tracked_after.append(month)
+            else:
+                tracked_after.append(month)
+
+    return tracked_before, [first_untracked, last_untracked], tracked_after
+
+
+@accept_duplicates
+def save_counts(query: str, month_range: list, before: list, after: list, percent=0.1):
+    # pull count of tweets for a month or range between two months
+
+    months_array = get_month_array(get_date_range(month_range))
+
     params = form_count_query_params(query, first, last)
+    counts = count_date_range(params)
 
-    # set 10 percent of total count as target
-    percent = 0.1
-    tracker = []
+    tracker = form_trackers(counts, percent)
+    db["counts"].insert_many(tracker)
+
+    full_months_array = before + months_array + after
+    db["counts"].update_one({"_id": "months"}, {"$set": {"months":  full_months_array}}, upsert=True)
+
+
+def count_date_range(params):
+    # repeat response call until entire date range collected as no next token in provided
+    counts = []
     response = count(params)
+    while "next_token" in response["meta"]:
+        params["next_token"] = response["meta"]["next_token"]
+        response = count(params)
+        new_counts = response["data"]
+        new_counts.reverse()
+        counts += new_counts
+    return counts
 
-    for day in response["data"]:
+
+def form_trackers(counts, percent):
+    tracker = []
+    for day in counts:
         tweet_count = day["tweet_count"]
+
         day_track = {
             "tweet_total": tweet_count,
             "tweet_target": round(tweet_count * percent),
@@ -40,10 +92,7 @@ def save_counts(query: str, months: list):
             "_id": twitter_date_format_to_day(day["start"])
         }
         tracker.append(day_track)
-
-    db["counts"].insert_many(tracker)
-    # todo update to run for multiple months
-    db["counts"].update_one({"track": "months"}, {"$addToSet": {"months": month}}, upsert=True)
+    return tracker
 
 
 def update_tracker(day: str, increment: int):
@@ -51,8 +100,63 @@ def update_tracker(day: str, increment: int):
     db["counts"].update_one({"_id": day}, {"$inc": {"tweet_current": increment}})
 
 
+def select_rand_time():
+    # todo set so not near start of day
+    hours, minutes, seconds = randrange(24), randrange(60), randrange(60)
+
+    rough_time_string = f"{hours}:{minutes}:{seconds}"
+
+    return datetime.strptime(rough_time_string, "%-H:%-M:%-S").strftime("%H:%M:%S")
+
+
+@time_func
+def select_unused_time(day):
+    used_times = db["counts"].find_one({"_id": day}, {"starts": 1, "ends": 1, "_id": 0})
+
+    while True:
+        selected_time = select_rand_time()
+        if used_times is None:
+            break
+        elif time_new(selected_time, used_times):
+            break
+
+    return selected_time
+
+
+def time_new(selected_time, used_times):
+    time_obj = datetime.strptime(selected_time, "%H:%M:%S")
+    start_of_day = datetime.strptime("00:30:00", "%H:%M:%S")
+
+    if time_obj < start_of_day:
+        return False
+
+    starts = used_times["starts"]
+    ends = used_times["ends"]
+    for x in range(starts):
+        if time_between(selected_time, starts[x], ends[x]):
+            return False
+    return True
+
+
+def time_between(time_obj, start, end):
+    start_obj = datetime.strptime(start, "%H:%M:%S")
+    end_obj = datetime.strptime(end, "%H:%M:%S")
+
+    if time_obj > start_obj:
+        return True
+    elif time_obj < end_obj: # todo test to see if will overlap
+        return True
+    else:
+        return False
+
+
+
+
 if __name__ == '__main__':
-    track_months("bitcoin", ["Jan17", "Jun21"])
+    # track_months("bitcoin", ["Jan17", "Jun21"])
     # update_tracker("2018-01-01", 100)
     # print(json.dumps(num, indent=4, sort_keys=False))
 
+    months = get_month_array(["Jan 21", "Jun 21"])
+    save_counts("bitcoin", months)
+    # db["counts"].update_one({"track": "months"}, {"$push": {"months": {"$each": months, "$position": -1}}}, upsert=True)
--- daily_twitter_sample/utils.py
+++ daily_twitter_sample/utils.py
@@ -1,64 +1,7 @@
-import json
 import calendar
-import os
 import time
 from datetime import datetime, timedelta, timezone
 from typing import List
-
-from pymongo import MongoClient
-from pymongo.errors import BulkWriteError
-
-
-def extract_env_vars():
-    m = os.environ.get("TWITTER_DATE").split(" ")
-    months_list = list(map(' '.join, zip(m[::2], m[1::2])))
-
-    mongo = MongoClient(os.environ.get('MONGO_CLIENT'))
-    q = os.environ.get('TWITTER_QUERY')
-    dbname = os.environ.get('TWITTER_DBNAME', q.split(" ")[0] + "_extended")  # todo maybe change
-
-    return months_list, q, mongo[dbname]
-
-
-months, query, db = extract_env_vars()
-
-
-def accept_duplicates(func):
-    # define a decorator to ignore duplicate _id errors
-
-    def wrap(*args, **kwargs):
-        try:
-            func(*args, **kwargs)
-        except BulkWriteError as e:
-            print("Duplicates present but ignoring error.")
-            pass
-
-    return wrap
-
-
-def json_print(func):
-    # Decorator that reports the execution time
-
-    def wrap(*args, **kwargs):
-        result = func(*args, **kwargs)
-        print(json.dumps(result, indent=4, sort_keys=False))
-        return result
-
-    return wrap
-
-
-def time_func(func):
-    # Decorator that reports the execution time
-
-    def wrap(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-
-        duration = end - start
-        return duration, result
-
-    return wrap
 
 
 def check_for_duplicates(dictionary_list, item):
@@ -74,14 +17,19 @@ def check_for_duplicates(dictionary_list, item):
         return True
 
 
-def twitter_date_format(date: str, end_of_day=False):
+def twitter_date_format(date: str, time_string=None, end_of_day=False):
     # convert date in '%Y-%m-%d' format into twitter date format
 
     date_obj = datetime.strptime(date, '%Y-%m-%d')
-    if end_of_day:
-        date_obj = date_obj.replace(hour=23, minute=59, second=59)
-    utc_date = date_obj.replace(tzinfo=timezone.utc)
+    if time_string is not None:
+        t = datetime.strptime(time, '%H:%M-%S')
+        date_obj = date_obj.replace(
+            hour=t.hour, minute=t.minute, second=t.second)
 
+    elif end_of_day:
+        date_obj = date_obj.replace(hour=23, minute=59, second=59)
+
+    utc_date = date_obj.replace(tzinfo=timezone.utc)
     return str(utc_date).replace(" ", "T")
 
 
@@ -113,8 +61,9 @@ def get_date_range(months: list):
         last_month = datetime.strptime(months[-1], "%b %y")
     else:
         last_month = first
-    last = calendar.monthrange(int(last_month.strftime("%y")),
-                               int(last_month.strftime("%m")))[1]
+
+    # set the last datetime to the last day in the month
+    last = calendar.monthrange(int(last_month.strftime("%y")), int(last_month.strftime("%m")))[1]
     last = datetime.strptime(f"{last} {months[-1]}", "%d %b %y")
 
     first = datetime.strftime(first, "%Y-%m-%d")
@@ -124,8 +73,9 @@ def get_date_range(months: list):
 
 
 # @json_print
-def get_date_array(first: str, last: str):
+def get_date_array(date_range: tuple):
     # get a list of dates from year and month name in '%Y-%m-%d' format
+    first, last = date_range
 
     current = datetime.strptime(first, "%Y-%m-%d")
     last = datetime.strptime(last, "%Y-%m-%d")
@@ -142,7 +92,33 @@ def get_date_array(first: str, last: str):
     return date_array
 
 
-if __name__ == "__main__":
-    first, last = get_date_range(["Jan 17"])
-    get_date_array(first, last)
+def get_month_array(month_range):
+    # returns first and last month with year for a specified range between 2 months with years
 
+    current_month = datetime.strptime(month_range[0], "%b %y")
+    last_month = datetime.strptime(month_range[-1], "%b %y")
+    month_array: List[str] = []
+    end_of_range = True
+
+    while end_of_range:
+        current_string = datetime.strftime(current_month, "%b %y")
+        month_array.append(current_string)
+
+        next_month = int(datetime.strftime(current_month, "%-m"))
+        next_year = int(datetime.strftime(current_month, "%Y"))
+
+        if next_month == 12:
+            next_month = 0
+            next_year += 1
+        next_month += 1
+
+        end_of_range = current_month != last_month
+        current_month = current_month.replace(month=next_month, year=next_year)
+
+    return month_array
+
+
+if __name__ == "__main__":
+    print(get_date_array(get_date_range(["Jan 17"])))
+
+    # months = get_month_array(["Jan 18", "Jun 21"])
