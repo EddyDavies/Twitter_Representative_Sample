609f197ae507e4c862064f744f01f6a6e98a5e28
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2021-07-30 00:41:00 +0100

    Fix bad import

--- src/track.py
+++ src/track.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from random import randrange
 
-from src.utils import fix_array_misalignment
 from utils import get_date_range, twitter_date_format_to_day, get_month_array, twitter_date_format_to_time
 from decorators import db, accept_duplicates
 from count_or_search import count, form_count_query_params
