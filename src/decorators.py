d9a3d562590bcb29e019fd3db1b743b9b5a07a22
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2021-08-03 17:56:38 +0100

    Add print of Env vars

--- src/decorators.py
+++ src/decorators.py
@@ -51,10 +51,14 @@ def extract_env_vars():
     m = os.environ.get("TWITTER_DATE").split(" ")
     months_list = list(map(' '.join, zip(m[::2], m[1::2])))
 
-    mongo = MongoClient(os.environ.get('MONGO_CLIENT'))
+    mongo_url = os.environ.get('MONGO_CLIENT')
+    mongo = MongoClient(mongo_url)
     q = os.environ.get('TWITTER_QUERY')
     dbname = os.environ.get('TWITTER_DBNAME')
     # todo maybe change
+
+    print(f"Query: {q}, DBName: {dbname}, Date: {months_list[0]} to {months_list[1]}")
+    print(mongo_url)
 
     return months_list, q, mongo[dbname], mongo["shared"]
 
