ea7d482b97c81f9571b3b6c62b5a49ec985203ea
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2021-09-23 01:29:23 +0100

    Update mongo.py

--- src/mongo.py
+++ src/mongo.py
@@ -3,17 +3,17 @@ from pymongo import MongoClient
 
 def extract_env_vars():
     dates_string = None
-    
+
     m = os.environ.get("TWITTER_MONTHS")
     if m:
         m = m.split(" ")
-        months = list(map(' '.join, zip(m[::2], m[1::2])))
-        dates_string = f"{months[0]} to {months[1]}"
+        m = list(map(' '.join, zip(m[::2], m[1::2])))
+        dates_string = f"{m[0]} to {m[1]}"
 
     d = os.environ.get("TWITTER_DATES")
     if m:
-        dates = d.split(" ")
-        dates_string = f"{dates[0]} to {dates[1]}"
+        d = d.split(" ")
+        dates_string = f"{d[0]} to {d[1]}"
 
     mongo_url = os.environ.get('MONGO_CLIENT')
     mongo = MongoClient(mongo_url)
@@ -24,7 +24,7 @@ def extract_env_vars():
     print(f"Query: {q}, DBName: {dbname}, Date: {dates_string}")
     print(mongo_url)
 
-    return dates, months, q, mongo[dbname], mongo["shared"]
+    return d, m, q, mongo[dbname], mongo["shared"]
 
 
 dates, months, query, db, db_share = extract_env_vars()
